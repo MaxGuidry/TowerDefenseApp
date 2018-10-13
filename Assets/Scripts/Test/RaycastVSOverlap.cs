c17ca4694794289fe290e62a70611d3b355d1af2
Max Guidry <mguidry7@outlook.com>
2018-10-13 13:29:24 -0500

    Started on tower placement

--- Assets/Scripts/Test/RaycastVSOverlap.cs
+++ Assets/Scripts/Test/RaycastVSOverlap.cs
@@ -2,15 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RaycastVSOverlap : MonoBehaviour {
+public class RaycastVSOverlap : MonoBehaviour
+{
+    public bool Raycast;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        StartCoroutine(RayCast());
+
+        StartCoroutine(Overlap());
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator RayCast()
+    {
+        while (true)
+        {
+            if (Raycast)
+            {
+                int i = 0;
+
+                while (i < 10000)
+                {
+                    Physics.Raycast(Vector3.zero, Vector3.forward, 10f);
+                    Physics.Raycast(Vector3.zero, Vector3.forward, 10f);
+                    Physics.Raycast(Vector3.zero, Vector3.forward, 10f);
+                    Physics.Raycast(Vector3.zero, Vector3.forward, 10f);
+                    Physics.Raycast(Vector3.zero, Vector3.forward, 10f);
+                    i++;
+                }
+            }
+            yield return null;
+        }
+    }
+
+    public IEnumerator Overlap()
+    {
+        while (true)
+        {
+            if (!Raycast)
+            {
+                int i = 0;
+                while (i < 10000)
+                {
+                    var c = Physics.OverlapBox(Vector3.zero, new Vector3(.5f, 2f, .5f), Quaternion.identity);
+                    i++;
+                }
+            }
+            yield return null;
+        }
+    }
 }
---
+++ Assets/Scripts/Tower/PlaceTowerAroundHub.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTowerAroundHub : MonoBehaviour
+{
+    public GameObject towerPrefab;
+    // Use this for initialization
+    void Start()
+    {
+
+        TESTOBJ = GameObject.Instantiate(towerPrefab);
+        rend = TESTOBJ.GetComponent<Renderer>();
+    }
+
+    bool canPlace;
+#if UNITY_EDITOR
+    Vector3 mouse => Input.mousePosition;
+    public GameObject TESTOBJ;
+#endif
+    public Camera cam;
+    public GameObject Hub;
+    // Update is called once per frame
+    void Update()
+    {
+
+        //in editor use mouse
+#if UNITY_EDITOR
+        canPlace = false;
+        var worldPoint = cam.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, (Hub.transform.position - cam.transform.position).magnitude));
+        TESTOBJ.transform.position = worldPoint;
+        var cols = Physics.OverlapBox(new Vector3(worldPoint.x, 0, worldPoint.z), new Vector3(towerPrefab.transform.localScale.x/2, 1, towerPrefab.transform.localScale.z/2));
+       
+        for(int i = 0;i<cols.Length;i++)
+        {
+            if(cols[i].gameObject.tag == "Tower"||cols[i].gameObject.tag == "Hub")
+            {
+                canPlace = false;
+                break;
+            }
+            if (cols[i].gameObject.tag == "Area")
+                canPlace = true;
+        }
+#endif
+        if (canPlace)
+            rend.material = Green;
+        else
+            rend.material = Red;
+    }
+    public Material Red;
+    public Material Green;
+    Renderer rend;
+}
