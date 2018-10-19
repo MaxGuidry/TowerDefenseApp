eaa97c4e868b9b8cf29ceebc0eb6aae811435648
Donray Williams <w.donray@yahoo.com>
2018-10-19 15:57:07 -0500

    Cleaning up scripts

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using GlobalMethods;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,16 +8,22 @@ namespace EnemyClass
 {
     public class EnemyManager : MonoBehaviour
     {
-        [Header("Hub")] public GameObject Center;
-        public float SpawnDistance;
-        [Header("Enemy Prefabs")] public GameObject BasicMeleePrefab;
-        public GameObject BasicRangedPrefab;
-        public GameObject BasicTankPrefab;
-
-        public List<Enemy> AllEnemies;
         private Dictionary<EnemyTypes, GameObject> _enemyDictionary;
 
-        private void Start()
+        public List<Enemy> AllEnemies;
+
+        [Header("Enemy Prefabs")] public GameObject BasicMeleePrefab;
+
+        public GameObject BasicRangedPrefab;
+        public GameObject BasicTankPrefab;
+        [Header("Hub")] public GameObject Center;
+        public float SpawnDistance;
+
+        /// <summary>
+        ///     Add each enemy type to a dictionary with the assigned prefab
+        ///     Start each coroutine
+        /// </summary>
+        private void Awake()
         {
             _enemyDictionary = new Dictionary<EnemyTypes, GameObject>
             {
@@ -27,20 +33,23 @@ namespace EnemyClass
             };
 
             StartCoroutine(Utils.RepeatAction(SpawnRandomEnemy, Random.Range(1, 2)));
+            StartCoroutine(Utils.RepeatAction(BeginEnemyAction, 0.02f));
         }
 
-        private void Update()
-        {
-            foreach (var enemy in AllEnemies)
-            {
-                if (enemy.CurrentState != EnemyState.Dead)
-                {
-                    enemy.ChaseTarget(Center.transform.position, Time.deltaTime);
-                }
-            }
-        }
-
-        public GameObject SpawnEnemy(float maxHealth, float enemyDamage, float enemySpeed, float stoppingDistance, EnemyTypes enemyType,
+        /// <summary>
+        ///     Create each enemy with the passed in random stat
+        ///     Add each enemy into the list
+        /// </summary>
+        /// <param name="maxHealth"></param>
+        /// <param name="enemyDamage"></param>
+        /// <param name="enemySpeed"></param>
+        /// <param name="stoppingDistance"></param>
+        /// <param name="enemyType"></param>
+        /// <param name="enemyPrefab"></param>
+        /// <param name="spawnPos"></param>
+        /// <returns></returns>
+        public GameObject SpawnEnemy(float maxHealth, float enemyDamage, float enemySpeed, float stoppingDistance,
+            EnemyTypes enemyType,
             GameObject enemyPrefab, Vector3 spawnPos)
         {
             var enemyInstance = Instantiate(_enemyDictionary[enemyType], spawnPos, Quaternion.identity);
@@ -53,41 +62,74 @@ namespace EnemyClass
             return enemyInstance;
         }
 
+        /// <summary>
+        ///     Set each random stat for the enemy
+        /// </summary>
         public void SpawnRandomEnemy()
         {
             var enumType = Utils.GetRandomEnum<EnemyTypes>();
-            float maxHealth, enemyDamage, enemySpeed;
             var spawnPos = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 0)) * SpawnDistance;
+            Utils.SetEnemyStats(enumType);
+            float maxHealth, enemyDamage, enemySpeed, stoppingDistance;
+            GameObject prefab;
 
             switch (enumType)
             {
                 case EnemyTypes.BasicMelee:
-                    {
-                        maxHealth = Random.Range(100, 150);
-                        enemyDamage = Random.Range(10, 20);
-                        enemySpeed = Random.Range(5, 8);
-                        SpawnEnemy(maxHealth, enemyDamage, enemySpeed, 1f, enumType, BasicMeleePrefab, spawnPos);
-                        break;
-                    }
+                {
+                    prefab = BasicMeleePrefab;
+                    maxHealth = Utils.BasicMelee.MaxHealth;
+                    enemyDamage = Utils.BasicMelee.EnemyDamage;
+                    enemySpeed = Utils.BasicMelee.EnemySpeed;
+                    stoppingDistance = Utils.BasicMelee.StoppingDistance;
+                    break;
+                }
                 case EnemyTypes.BasicRanged:
-                    {
-                        maxHealth = Random.Range(75, 100);
-                        enemyDamage = Random.Range(25, 30);
-                        enemySpeed = Random.Range(5, 8);
-                        SpawnEnemy(maxHealth, enemyDamage, enemySpeed, 10f, enumType, BasicRangedPrefab, spawnPos);
-                        break;
-                    }
+                {
+                    prefab = BasicRangedPrefab;
+                    maxHealth = Utils.BasicRanged.MaxHealth;
+                    enemyDamage = Utils.BasicRanged.EnemyDamage;
+                    enemySpeed = Utils.BasicRanged.EnemySpeed;
+                    stoppingDistance = Utils.BasicRanged.StoppingDistance;
+                    break;
+                }
                 case EnemyTypes.BasicTank:
-                    {
-                        maxHealth = Random.Range(200, 250);
-                        enemyDamage = Random.Range(5, 20);
-                        enemySpeed = Random.Range(2, 3);
-                        SpawnEnemy(maxHealth, enemyDamage, enemySpeed, 1f, enumType, BasicTankPrefab, spawnPos);
-                        break;
-                    }
+                {
+                    prefab = BasicTankPrefab;
+                    maxHealth = Utils.BasicTank.MaxHealth;
+                    enemyDamage = Utils.BasicTank.EnemyDamage;
+                    enemySpeed = Utils.BasicTank.EnemySpeed;
+                    stoppingDistance = Utils.BasicRanged.StoppingDistance;
+                    break;
+                }
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+
+            SpawnEnemy(maxHealth, enemyDamage, enemySpeed, stoppingDistance, enumType, prefab, spawnPos);
+        }
+
+        /// <summary>
+        ///     Using this action to make each enemy chase the target
+        /// </summary>
+        /// <returns></returns>
+        private void BeginEnemyAction()
+        {
+            foreach (var enemy in AllEnemies)
+            {
+                if (enemy.CurrentState != EnemyState.Dead)
+                {
+                    enemy.ChaseTarget(Center.transform.position, Time.deltaTime);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Stop all Cotoutines on this behaviour
+        /// </summary>
+        private void OnDisable()
+        {
+            StopAllCoroutines();
         }
     }
 }
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,29 +1,82 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
+using EnemyClass;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Utils : MonoBehaviour
+namespace GlobalMethods
 {
-    public static T GetRandomEnum<T>()
+    public class Utils : MonoBehaviour
     {
-        var values = Enum.GetValues(typeof(T));
-        var enumType = values.GetValue(Random.Range(0, values.Length));
-        return (T)enumType;
-    }
+        public static Stats BasicMelee, BasicRanged, BasicTank;
 
-    public static float UpdateHealth(float newHealth)
-    {
-        return newHealth;
-    }
-
-    public static IEnumerator RepeatAction(Action func, int delay)
-    {
-        while (true)
+        /// <summary>
+        ///     Returns a random enum
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetRandomEnum<T>()
         {
-            func();
-            yield return new WaitForSeconds(delay);
+            var values = Enum.GetValues(typeof(T));
+            var enumType = values.GetValue(Random.Range(0, values.Length));
+            return (T) enumType;
         }
+
+        /// <summary>
+        ///     Repeat any action an amount of times
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="delay"></param>
+        /// <returns></returns>
+        public static IEnumerator RepeatAction(Action func, float delay)
+        {
+            while (true)
+            {
+                func();
+                yield return new WaitForSeconds(delay);
+            }
+        }
+
+        /// <summary>
+        ///     Set each enemy stats here
+        /// </summary>
+        /// <param name="type"></param>
+        public static void SetEnemyStats(EnemyTypes type)
+        {
+            switch (type)
+            {
+                case EnemyTypes.BasicMelee:
+                {
+                    BasicMelee.MaxHealth = Random.Range(100, 150);
+                    BasicMelee.EnemyDamage = Random.Range(10, 20);
+                    BasicMelee.EnemySpeed = Random.Range(5, 8);
+                    BasicMelee.StoppingDistance = 1f;
+                    break;
+                }
+                case EnemyTypes.BasicRanged:
+                {
+                    BasicRanged.MaxHealth = Random.Range(75, 100);
+                    BasicRanged.EnemyDamage = Random.Range(25, 30);
+                    BasicRanged.EnemySpeed = Random.Range(5, 8);
+                    BasicRanged.StoppingDistance = 7f;
+                    break;
+                }
+                case EnemyTypes.BasicTank:
+                {
+                    BasicTank.MaxHealth = Random.Range(200, 250);
+                    BasicTank.EnemyDamage = Random.Range(5, 20);
+                    BasicTank.EnemySpeed = Random.Range(2, 3);
+                    BasicTank.StoppingDistance = 1f;
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+        }
+    }
+
+    public struct Stats
+    {
+        public float MaxHealth, EnemyDamage, EnemySpeed, StoppingDistance;
     }
 }
