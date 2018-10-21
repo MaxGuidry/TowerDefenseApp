6707148f961f6bfa1d3f7cdfab459b4db0df166f
Max Guidry <mguidry7@outlook.com>
2018-10-20 21:46:26 -0500

    Clean up for meeting

--- Assets/Scripts/Tower/PlaceTowerAroundHub.cs
+++ Assets/Scripts/Tower/PlaceTowerAroundHub.cs
@@ -4,50 +4,71 @@ using UnityEngine;
 
 public class PlaceTowerAroundHub : MonoBehaviour
 {
-    public GameObject towerPrefab;
-    // Use this for initialization
+
+
     void Start()
     {
-
-        TESTOBJ = GameObject.Instantiate(towerPrefab);
-        rend = TESTOBJ.GetComponent<Renderer>();
+        BuyTower();
     }
 
+    public GameObject towerPrefab;
     bool canPlace;
-#if UNITY_EDITOR
-    Vector3 mouse => Input.mousePosition;
-    public GameObject TESTOBJ;
-#endif
+
+    Vector3 screenPos;
+
     public Camera cam;
     public GameObject Hub;
-    // Update is called once per frame
-    void Update()
+    [HideInInspector]
+    public GameObject newTower;
+
+    Renderer rend;
+    public void BuyTower()
+    {
+        newTower = GameObject.Instantiate(towerPrefab);
+        rend = newTower.GetComponent<Renderer>();
+        StartCoroutine(PlaceTower());
+    }
+    public IEnumerator PlaceTower()
     {
 
-        //in editor use mouse
-#if UNITY_EDITOR
-        canPlace = false;
-        var worldPoint = cam.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, (Hub.transform.position - cam.transform.position).magnitude));
-        TESTOBJ.transform.position = worldPoint;
-        var cols = Physics.OverlapBox(new Vector3(worldPoint.x, 0, worldPoint.z), new Vector3(towerPrefab.transform.localScale.x/2, 1, towerPrefab.transform.localScale.z/2));
-       
-        for(int i = 0;i<cols.Length;i++)
+        bool placed = false;
+        while (!placed)
         {
-            if(cols[i].gameObject.tag == "Tower"||cols[i].gameObject.tag == "Hub")
-            {
-                canPlace = false;
-                break;
-            }
-            if (cols[i].gameObject.tag == "Area")
-                canPlace = true;
-        }
+#if UNITY_EDITOR
+            screenPos = Input.mousePosition;
 #endif
-        if (canPlace)
-            rend.material = Green;
-        else
-            rend.material = Red;
+            canPlace = false;
+            var worldPoint = cam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, (Hub.transform.position - cam.transform.position).magnitude));
+            newTower.transform.position = worldPoint;
+            var cols = Physics.OverlapBox(new Vector3(worldPoint.x, 0, worldPoint.z), new Vector3(towerPrefab.transform.localScale.x / 2, 1, towerPrefab.transform.localScale.z / 2));
+
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if (cols[i].gameObject == newTower)
+                    continue;
+                if (cols[i].gameObject.tag == "Tower" || cols[i].gameObject.tag == "Hub")
+                {
+                    canPlace = false;
+                    break;
+                }
+                if (cols[i].gameObject.tag == "Area")
+                    canPlace = true;
+            }
+            //TODO: change to button for placing.
+            if (canPlace && Input.GetMouseButton(0))
+            {
+                placed = true;
+            }
+
+            if (canPlace)
+                rend.material.color = Color.green;
+            else
+                rend.material.color = Color.red;
+            yield return null;
+        }
+        rend.material.color = Color.blue;
+
+        //TODO: REMOVE WHEN BUYING IS ACTUALLY SET UP THIS IS FOR TESTING
+        BuyTower();
     }
-    public Material Red;
-    public Material Green;
-    Renderer rend;
 }
--- Assets/Scripts/Tower/TowerBehavior.cs
+++ Assets/Scripts/Tower/TowerBehavior.cs
@@ -4,60 +4,73 @@ using UnityEngine;
 
 namespace Tower
 {
+    //TODO: towers target enemies based on stats.
     public class TowerBehavior : MonoBehaviour
     {
-
+        public GameObject mainTarget;
+        public Collider[] targets;
         public TowerData data;
         public GameObject bullet;
         void Awake()
         {
-            bullets = new List<Quaternion>(data.bulletCount);
-            for (int i = 0; i < data.bulletCount; i++)
-                bullets.Add(Quaternion.Euler(Vector3.zero));
-        }
 
+        }
+        private void Start()
+        {
+
+            if (data.type == TowerData.TowerType.SPREAD)
+                StartCoroutine(FireSpread(.5f));
+            else
+                StartCoroutine(Fire(.5f, mainTarget));
+
+        }
         // Update is called once per frame
         void Update()
         {
-            data.targets = Physics.OverlapSphere(transform.position, data.range);
-            Debug.Log("oiu");
-            if (Input.GetKeyDown(KeyCode.Space))
-                if (data.type == TowerData.TowerType.SPREAD)
-                    StartCoroutine(FireSpread(0));
-                else
-                    StartCoroutine(Fire(0, data.mainTarget));
-            for (int i = 0; i < data.targets.Length; i++)
+            targets = Physics.OverlapSphere(transform.position, data.range, ~(1 << LayerMask.NameToLayer("Bullet") | 1 << LayerMask.NameToLayer("Tower")));
+
+
+            for (int i = 0; i < targets.Length; i++)
             {
-                if (data.targets[i].gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                if (targets[i].gameObject.layer == LayerMask.NameToLayer("Enemy"))
                 {
-                    data.mainTarget = data.targets[i].gameObject;
+                    mainTarget = targets[i].gameObject;
                     break;
                 }
                 else
-                    data.mainTarget = null;
+                    mainTarget = null;
             }
         }
 
         IEnumerator Fire(float rate, GameObject target)
         {
-            yield return new WaitForSeconds(rate);
-            if (data.mainTarget == null)
-                yield break;
-            transform.LookAt(data.mainTarget.transform);
-            GameObject b = Instantiate(bullet, transform.position, transform.rotation, transform);
-            b.transform.rotation = Quaternion.RotateTowards(b.transform.rotation, Random.rotation, data.spreadAngle);
-            b.GetComponent<Rigidbody>().AddForce(b.transform.forward * data.bulletFireVel);
-        }
-
-        List<Quaternion> bullets;
-        IEnumerator FireSpread(float rate)
-        {
-            yield return new WaitForSeconds(rate);
-            foreach (Quaternion quat in bullets)
+            while (true)
             {
+                yield return new WaitForSeconds(rate);
+                while (mainTarget == null)
+                    yield return null;
+                transform.LookAt(mainTarget.transform);
                 GameObject b = Instantiate(bullet, transform.position, transform.rotation, transform);
                 b.transform.rotation = Quaternion.RotateTowards(b.transform.rotation, Random.rotation, data.spreadAngle);
                 b.GetComponent<Rigidbody>().AddForce(b.transform.forward * data.bulletFireVel);
+            }
+        }
+
+        IEnumerator FireSpread(float rate)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(rate);
+                while (mainTarget == null)
+                {
+                    yield return null;
+                }
+                for (int i = 0; i < data.bulletCount; i++)
+                {
+                    GameObject b = Instantiate(bullet, transform.position, transform.rotation, transform);
+                    b.transform.rotation = Quaternion.RotateTowards(b.transform.rotation, Random.rotation, data.spreadAngle);
+                    b.GetComponent<Rigidbody>().AddForce(b.transform.forward * data.bulletFireVel);
+                }
             }
         }
         public void Upgrade()
