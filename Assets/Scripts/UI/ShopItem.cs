285eceeed9244c5017f0036cd9517cc5e0886a72
Donray Williams <w.donray@yahoo.com>
2018-11-04 22:51:51 -0600

    Broken

---
+++ Assets/Scripts/UI/ShopItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tower;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    public GameObject Tower;
+    private TowerBehavior TowerBehavior => Tower.GetComponent<TowerBehavior>();
+    private Text ItemName => GetComponentInChildren<Text>();
+    private Image ItemImage => GetComponentInChildren<Image>();
+    private Button BuyItem => GetComponentInChildren<Button>();
+    private static PlaceTowerAroundHub PlaceTower => FindObjectOfType<PlaceTowerAroundHub>();
+
+    // Use this for initialization
+    private void Awake()
+    {
+        var towerName = TowerBehavior.data.TowerName ?? "No Name";
+        ItemName.text = towerName;
+        ItemImage.sprite = TowerBehavior.data.TowerSprite;
+        BuyItem.GetComponentInChildren<Text>().text = TowerBehavior.data.buyCost.ToString();
+        BuyItem.onClick.AddListener(() => PlaceTower.BuyTower(Tower));
+    }
+}
